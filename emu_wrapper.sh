c79322f35a08b7c7dcc3778e35cc06036df362dd
Marcelo Navarro <marcelo.nd.mx@gmail.com>
2023-09-20 18:22:33 +0200

    Replaced separate running of emu for concat version of fastq files

--- emu_wrapper.sh
+++ emu_wrapper.sh
@@ -23,30 +23,37 @@ do files=$(shopt -s nullglob dotglob; echo $bc_dir/*);
 # if "files" variable contains something
 if (( ${#files} ));
 # create "fastaq" directory
-mkdir $bc_dir/fastaq
+mkdir $bc_dir/fastq
 then echo "Unzipping files in: $bc_dir";
 #echo $files
 for f in $bc_dir/*.gz; do
 # Run gunzip, retain files and extract in "fastaq" directory
   STEM=$(basename "${f}" .gz)
-  gunzip -c "${f}" > $bc_dir/fastaq/"${STEM}"
+  gunzip -c "${f}" > $bc_dir/fastq/"${STEM}"
 done
+# Concat fastq files
+#echo {$bc_dir}
+a="$bc_dir"
+echo ${a: -9:9}
+cat $bc_dir/fastq/* > $bc_dir/fastq/"${a: -9:9}_concat.fastq"
 fi
 done
 # Iterate over the list of barcode directories to run emu.
 for bc_dir in $barcode_dir_list;
 do echo $bc_dir;
 # If barcode/fastaq directory exists
-if [ -d "$bc_dir/fastaq/" ];
+if [ -d "$bc_dir/fastq/" ];
 # get the list of fastq files
-then export barcode_files=$bc_dir/fastaq/*;
+#then export barcode_files=$bc_dir/fastaq/*;
 #echo $barcode_files;
-for fq_file in $barcode_files;
-do echo $fq_file;
+#for fq_file in $barcode_files;
+#do echo $fq_file;
 # RUN emu!!!
+then fq_file=$bc_dir/fastq/*_concat.fastq;
+echo $fq_file;
 emu abundance $fq_file --output-dir $3 --keep-counts;
-done;
-# If the fastaq folder doesnot exist tell the user.
-else echo "fastaq folder does not exist"
+#done;
+# If the fastq folder doesnot exist tell the user.
+else echo "fastq folder does not exist";
 fi
 done
